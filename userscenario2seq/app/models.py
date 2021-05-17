715774840d752fc159b9beb94e1a046c46f9d4d6
imeldaafp <imeldaafp@gmail.com>
2021-05-17 13:04:20 +0700

    inisialisasi db

--- userscenario2seq/app/models.py
+++ userscenario2seq/app/models.py
@@ -31,5 +31,26 @@ class scenario(models.Model):
     date_created = models.DateTimeField()
     last_updated = models.DateTimeField()
 
+class project(models.Model):
+    id_project = models.IntegerField(default=0)
+    project_name = models.CharField(max_length=20)
+    project_desc = models.TextField()
+    date_created = models.DateTimeField ()
+    last_updated = models.DateTimeField()
+
+class feature(models.Model):
+    project = models.ForeignKey(project, on_delete=models.CASCADE)
+    id_feature = models.IntegerField(default=0)
+    feature_name = models.CharField(max_length=20)
+    user_story = model.CharField(max_length=100)
+    date_created = models.DateTimeField()
+    last_updated = models.DateTimeField()
+
+class scenario(models.Model):
+    feature = models.ForeignKey(feature, on_delete=models.CASCADE)
+    id_scenario = models.IntegerField(default=0)
+    tipe = models.CharField(max_length=25)
+    content = models.CharField(max_length=75)
+
 # Create your models here.
 
