2944c80e3c6ca482d4b5728630adbf54ad1c0c48
farisfasyadin <ffasyadin@gmail.com>
2021-05-20 21:52:25 +0700

    .

--- userscenario2seq/app/views.py
+++ userscenario2seq/app/views.py
@@ -58,7 +58,6 @@ def listProject(request):
     
     context = {}
     context['segment'] = 'listProject'
-
     context['project'] = project.objects.filter(id_user=request.user.id)
     context['user'] = request.user
 
