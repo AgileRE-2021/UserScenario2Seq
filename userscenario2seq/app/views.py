0a74891fb22bdc62521cff1f1b6ab063778ab68f
farisfasyadin <ffasyadin@gmail.com>
2021-05-24 13:38:19 +0700

    view

--- userscenario2seq/app/views.py
+++ userscenario2seq/app/views.py
@@ -10,6 +10,7 @@ from django.template import loader
 from django.http import HttpResponse
 from django import template
 from app.models import project, feature, scenario
+from django.utils import timezone
 
 @login_required(login_url="/login/")
 def index(request):
@@ -57,33 +58,34 @@ def createProject(request):
 def listProject(request):
     
     context = {}
+    
     context['segment'] = 'listProject'
-
-    context['project'] = project.objects.filter(id_user=request.user.id)
+    
+    context['project'] = project.objects.filter(id_user=request.user.id) #ambil record project dari model
+    #request.user adalah user yang sedang login
+    
     context['user'] = request.user
+    #segment, user, dan project adalah key dari object context
 
     return render(request, 'main/list-project.html', {'context': context})
 
 @login_required(login_url="/login/")
 def deleteProject(request, project_id):
     project_to_delete = get_object_or_404(project, pk=project_id).delete()
-    return redirect('list-project')
+    return redirect('list-project') #list-project adalah name dari url
 
 
 @login_required(login_url="/login/")
 def detailProject(request, project_id):
     
     context = {}
-    context['id'] = project_id
-    context['name'] = "Ini nama project"
-    context['array'] = [project_id, "Ini nama project"]
+    #project_to_edit = get_object_or_404(project, pk=project_id)
 
-    '''
-    context = {
-        segment: 'index',
-    }
-    '''
-
+    #project_to_edit.project_desc = "ini deskripsi baruu banget"
+    #project_to_edit.last_updated = timezone.now()
+    #project_to_edit.save()
+    
+    context['project'] = get_object_or_404(project, pk=project_id)
 
     #html_template = loader.get_template( 'main/detail-project.html' )
     return render(request, 'main/detail-project.html', {'context': context})
@@ -102,8 +104,14 @@ def addFeature(request, project_id):
 def editFeature(request, project_id, feature_id):
     
     context = {}
-    context['idProject'] = project_id
-    context['idFeature'] = feature_id
+    
+    #feature_to_update = get_object_or_404(feature, pk=feature_id)
+    #scenarios = feature.objects.filter(feature = feature_to_update)
+    #for s in scenarios: 
+        #s.content = request.
+    
+    #context['idProject'] = project_id
+    #context['idFeature'] = feature_id
   
     #html_template = loader.get_template( 'main/detail-project.html' )
     return render(request, 'main/edit-feature.html', {'context': context})
