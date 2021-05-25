ced5622bf6c526f73dd2b9c086b180464ef80d6e
farisfasyadin <ffasyadin@gmail.com>
2021-05-25 12:20:28 +0700

    hapus debug

--- userscenario2seq/core/settings.py
+++ userscenario2seq/core/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'app',  # Enable the inner app 
 ]
 
@@ -43,7 +42,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
